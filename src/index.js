ad7ec384f2949e6573930fa945f6930a20fbef84
thipereira02 <thiago.spereira02@gmail.com>
2021-08-27 19:30:49 -0300

    Moving folder styles to src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom";
 import React from "react";
 
+import "./styles/reset.css";
+import "./styles/main.css";
+
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector(".root"));
