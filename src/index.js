387bacb43ac7d0f1cc0413e53d318bf3164be515
Sidney Outeiro <sid@dcc.ufrj.br>
2021-08-31 17:45:11 -0300

    preparando para o review

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-
-import "./styles/reset.css";
-import "./styles/main.css";
-
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.querySelector(".root"));
